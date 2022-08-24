4916b90ff60fd8687f1aa6ec8c02a5989794907a
JacquelineMalatare <jacqueline.malatare@gmail.com>
2022-08-23 19:29:03 -0600

    update

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
-// Your JavaScript code goes here!
-
-
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
+const articleHearts = document.querySelectorAll(".like-glyph");
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(){
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.style.color = "red";
+        heart.className = "activated-heart";
       } else {
-        resolve("Pretend remote server notified of action!");
+        heart.innerText = EMPTY_HEART;
+        heart.style.color = "";
+        heart.className = "";
       }
-    }, 300);
-  });
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
 }
